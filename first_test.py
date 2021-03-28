2948ecf3b5faaeaf2eee3c7cbf56389c5aca4667
ilya0822 <kinarho@mail.ru>
2021-03-28 21:28:58 +0300

    2

--- first_test.py
+++ first_test.py
@@ -8,12 +8,31 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.action_chains import ActionChains
 
 
-
 def func_prin(message):
     print(message)
 
+driver = webdriver.Chrome()
 
-#waituntil - element visible, exist, displayed - conditions, wait custom conditions
+@pytest.fixture(scope="module")
+def login(request):
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+    driver.get("http://localhost:8080/")
+    login: WebElement = driver.find_element_by_xpath(
+        "//*[@class='inputOutside__input--1Sg9p'][@type='text']").send_keys('default')
+    password: WebElement = driver.find_element_by_xpath(
+        "//*[@class='inputOutside__input--1Sg9p'][@type='password']").send_keys('1q2w3e')
+    ingress: WebElement = driver.find_element_by_xpath("//*[@type='submit']").click()
+    yield
+    driver.quit()
+
+@pytest.fixture()
+def start_page():
+    driver.get("http://localhost:8080/ui/#default_personal/dashboard")
+
+
+# waituntil - element visible, exist, displayed - conditions, wait custom conditions
+#
 # def test_first():
 #    driver = webdriver.Chrome()
 #    driver.get("http://localhost:8080/ui/#login")
@@ -21,35 +40,19 @@ def func_prin(message):
 #    assert elem.is_displayed()
 
 
-def test_1():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(10)
-    driver.maximize_window()
-    driver.get("http://localhost:8080/")
-    login: WebElement = driver.find_element_by_xpath("//*[@class='inputOutside__input--1Sg9p'][@type='text']").send_keys('default')
-    password: WebElement = driver.find_element_by_xpath("//*[@class='inputOutside__input--1Sg9p'][@type='password']").send_keys('1q2w3e')
-    ingress: WebElement = driver.find_element_by_xpath("//*[@type='submit']").click()
+def test_1(start_page, login):
     demo: WebElement = driver.find_element_by_xpath('//*[@class="gridCell__grid-cell--3e2mS gridCell__align-left--2beIG dashboardTable__name--1sWJs"]').send_keys(Keys.ENTER)
     widget: WebElement = driver.find_element_by_xpath('(//div/button)[2]').click()
-    ingress1: WebElement = driver.find_element_by_xpath('(//label)[14]').click()
-    ingress2: WebElement = driver.find_element_by_xpath('(//div/button)[7]')
-    driver.execute_script("arguments[0].click();", ingress2)
+    button_passing: WebElement = driver.find_element_by_xpath('(//label)[14]').click()
+    button_next_step: WebElement = driver.find_element_by_xpath('(//div/button)[7]')
+    driver.execute_script("arguments[0].click();", button_next_step)
     DEMO_FILTER: WebElement = driver.find_element_by_xpath('//input[@aria-autocomplete="list"]').send_keys('DEMO_FILTER')
-    ingress4: WebElement = driver.find_element_by_xpath('//*[@class="ghostButton__icon--2rRly ghostButton__icon-at-right--3Ui7m"]').click()
-    ingress5: WebElement = driver.find_element_by_xpath('//*[@class="bigButton__big-button--ivY7j bigButton__color-booger--2IfQT"]').click()
+    button_next_step_DEMO_FILTER: WebElement = driver.find_element_by_xpath('//*[@class="ghostButton__icon--2rRly ghostButton__icon-at-right--3Ui7m"]').click()
+    ADD_NEW_WIDGET: WebElement = driver.find_element_by_xpath('//*[@class="bigButton__big-button--ivY7j bigButton__color-booger--2IfQT"]').click()
     assert driver.find_element_by_xpath('//*[@class="noDataAvailable__no-data-available--3EH6v"]')
 
 
-
-
-def test_2():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(10)
-    driver.maximize_window()
-    driver.get("http://localhost:8080/")
-    login: WebElement = driver.find_element_by_xpath("//*[@class='inputOutside__input--1Sg9p'][@type='text']").send_keys('default')
-    password: WebElement = driver.find_element_by_xpath("//*[@class='inputOutside__input--1Sg9p'][@type='password']").send_keys('1q2w3e')
-    ingress: WebElement = driver.find_element_by_xpath("//*[@type='submit']").click()
+def test_2(start_page, login):
     launches: WebElement = driver.find_element_by_xpath('//a[@href="#default_personal/launches"]').click()
     skipped_main: WebElement = int(driver.find_element_by_xpath('//*[@href="#default_personal/launches/all/7?item0Params=filter.eq.hasStats%3Dtrue%26filter.eq.hasChildren%3Dfalse%26filter.in.type%3DSTEP%26filter.in.status%3DSKIPPED"]').text)
     failed_main: WebElement = int(driver.find_element_by_xpath('//*[@href="#default_personal/launches/all/7?item0Params=filter.eq.hasStats%3Dtrue%26filter.eq.hasChildren%3Dfalse%26filter.in.type%3DSTEP%26filter.in.status%3DFAILED%252CINTERRUPTED"]').text)
@@ -61,19 +64,19 @@ def test_2():
     skipped = 0
     total = 0
     while True:
-        total1 = len(driver.find_elements_by_xpath("//*[@class='gridRow__grid-row--1pS-5']"))
-        skipped1 = len(driver.find_elements_by_xpath("//span[text()='Skipped'][@class='itemInfo__mobile-status--1qoKx']"))
-        failed1 = len(driver.find_elements_by_xpath("//*[@class='gridRow__grid-row-wrapper--1dI9K stepGrid__failed--2d38k']"))
-        passed1 = len(driver.find_elements_by_xpath("//span[text()='Passed'] [@class='itemInfo__mobile-status--1qoKx']"))
+        total_len = len(driver.find_elements_by_xpath("//*[@class='gridRow__grid-row--1pS-5']"))
+        skipped_len = len(driver.find_elements_by_xpath("//span[text()='Skipped'][@class='itemInfo__mobile-status--1qoKx']"))
+        failed_len = len(driver.find_elements_by_xpath("//*[@class='gridRow__grid-row-wrapper--1dI9K stepGrid__failed--2d38k']"))
+        passed_len = len(driver.find_elements_by_xpath("//span[text()='Passed'] [@class='itemInfo__mobile-status--1qoKx']"))
 
-        element_1: WebElement = driver.find_element_by_css_selector('.pageButtons__page-buttons--uRADS '
+        button: WebElement = driver.find_element_by_css_selector('.pageButtons__page-buttons--uRADS '
                                                                     '.pageButton__page-button--OHYKN:nth-child(6)')
-        classes_element = element_1.get_attribute('class')
-        driver.execute_script("arguments[0].click();", element_1)
-        total += total1
-        passed += passed1
-        failed += failed1
-        skipped += skipped1
+        classes_element = button.get_attribute('class')
+        driver.execute_script("arguments[0].click();", button)
+        total += total_len
+        passed += passed_len
+        failed += failed_len
+        skipped += skipped_len
         if classes_element == 'pageButton__page-button--OHYKN pageButton__disabled--3-RYj':
             break
     assert total_main == total
@@ -82,48 +85,25 @@ def test_2():
     assert failed_main == failed
 
 
-def test_3():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(10)
-    driver.maximize_window()
-    driver.get("http://localhost:8080/")
-    login: WebElement = driver.find_element_by_xpath("//*[@class='inputOutside__input--1Sg9p'][@type='text']").send_keys('default')
-    password: WebElement = driver.find_element_by_xpath("//*[@class='inputOutside__input--1Sg9p'][@type='password']").send_keys('1q2w3e')
-    ingress: WebElement = driver.find_element_by_xpath("//*[@type='submit']").click()
+def test_3(start_page, login):
     launches: WebElement = driver.find_element_by_xpath('//a[@href="#default_personal/launches"]').click()
-    total_1 = len(driver.find_elements_by_xpath("//*[@class='gridRow__grid-row-wrapper--1dI9K']"))
-    assert total_1 == 10
+    total = len(driver.find_elements_by_xpath("//*[@class='gridRow__grid-row-wrapper--1dI9K']"))
+    assert total == 10
 
 
-
-def test_4():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(10)
-    driver.maximize_window()
-    driver.get("http://localhost:8080/")
-    login: WebElement = driver.find_element_by_xpath("//*[@class='inputOutside__input--1Sg9p'][@type='text']").send_keys('default')
-    password: WebElement = driver.find_element_by_xpath("//*[@class='inputOutside__input--1Sg9p'][@type='password']").send_keys('1q2w3e')
-    ingress: WebElement = driver.find_element_by_xpath("//*[@type='submit']").click()
-    ingress1: WebElement = driver.find_element_by_xpath('//*[@class="userBlock__menu-icon--_7G3G"]').click()
-    ingress2: WebElement = driver.find_element_by_xpath('//a[@href="#user-profile"][@class="userBlock__menu-item--6Fruf"]').click()
-    ingress3: WebElement = driver.find_element_by_xpath('//*[@class="inputDropdown__select-block--2CQf-"]').click()
-    ingress4: WebElement = driver.find_element_by_xpath('//*[@class="inputDropdownOption__dropdown-option--3Szk3"][2]').click()
-    ingress5: WebElement = driver.find_element_by_xpath('// span[ @class ="inputDropdown__value--2gB2s"]').text
-    assert ingress5 == 'Русский'
+def test_4(start_page, login):
+    drop_buttons: WebElement = driver.find_element_by_xpath('//*[@class="userBlock__menu-icon--_7G3G"]').click()
+    PROFILE: WebElement = driver.find_element_by_xpath('//a[@href="#user-profile"][@class="userBlock__menu-item--6Fruf"]').click()
+    Language: WebElement = driver.find_element_by_xpath('//*[@class="inputDropdown__select-block--2CQf-"]').click()
+    Russian: WebElement = driver.find_element_by_xpath('//*[@class="inputDropdownOption__dropdown-option--3Szk3"][2]').click()
+    Russian_text: WebElement = driver.find_element_by_xpath('// span[ @class ="inputDropdown__value--2gB2s"]').text
+    assert Russian_text == 'Русский'
 
 
-
-def test_5():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(10)
-    driver.maximize_window()
-    driver.get("http://localhost:8080/")
-    login: WebElement = driver.find_element_by_xpath("//*[@class='inputOutside__input--1Sg9p'][@type='text']").send_keys('default')
-    password: WebElement = driver.find_element_by_xpath("//*[@class='inputOutside__input--1Sg9p'][@type='password']").send_keys('1q2w3e')
-    ingress: WebElement = driver.find_element_by_xpath("//*[@type='submit']").click()
-    ingress1: WebElement = driver.find_element_by_xpath('//*[@class="userBlock__menu-icon--_7G3G"]').click()
-    ingress2: WebElement = driver.find_element_by_xpath('//a[@href="#user-profile"][@class="userBlock__menu-item--6Fruf"]').click()
-    ingress3: WebElement = driver.find_element_by_xpath('//input[@type="file"]').send_keys("C:/Users/User/Pictures/zxc.jpg")
+def test_5(start_page, login):
+    drop_buttons: WebElement = driver.find_element_by_xpath('//*[@class="userBlock__menu-icon--_7G3G"]').click()
+    PROFILE: WebElement = driver.find_element_by_xpath('//a[@href="#user-profile"][@class="userBlock__menu-item--6Fruf"]').click()
+    Photo: WebElement = driver.find_element_by_xpath('//input[@type="file"]').send_keys("C:/Users/User/Pictures/zxc.jpg")
 
 
 
